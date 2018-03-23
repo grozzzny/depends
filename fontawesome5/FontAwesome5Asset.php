be9af7c69e157c849d9540620d97008f4f91b997
Dmitriy Groznyy <grozzzny@list.ru>
2018-03-23 16:51:47 +0200

    add depends

--- fontawesome5/FontAwesome5Asset.php
+++ fontawesome5/FontAwesome5Asset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class FontAwesome5Asset extends AssetBundle
 {
-    public $sourcePath = '@bower/components/font-awesome';
+    public $sourcePath = '@vendor/components/font-awesome';
     public $css = [
         'css/fontawesome-all.css',
     ];
