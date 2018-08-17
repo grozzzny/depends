79f59c7916ee458334e43d5e210a7968f8af4faf
grozzzny <1234578>
2018-08-17 18:19:57 +0200

    add files

--- sweetalert/SweetalertAsset.php
+++ sweetalert/SweetalertAsset.php
@@ -5,9 +5,9 @@ class SweetalertAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = '@grozzzny/depends/sweetalert';
     public $css = [
-        'sweetalert.css',
+        'sweetalert2.min.css',
     ];
     public $js = [
-        'sweetalert.min.js',
+        'sweetalert2.min.js',
     ];
 }
