58cd79555342cd8489a8a4af1216fe639307947d
grozzzny <grozzzny@list.ru>
2020-06-23 15:41:48 +0200

    fix widget images

---
+++ glyphicon/GlyphiconAsset.php
@@ -0,0 +1,11 @@
+<?php
+namespace grozzzny\depends\glyphicon;
+
+class GlyphiconAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@grozzzny/depends/glyphicon';
+
+    public $css = [
+        'glyphicons.css'
+    ];
+}
